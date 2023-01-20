68fccbdc739d0d70835217fc29868e453ca001d3
Crowdbotics <engineering@crowdbotics.com>
2023-01-20 17:06:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-sea-dev-58369",
+  url: "https://wandering-sea-dev-58369.botics.co",
+  api: "https://wandering-sea-dev-58369.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
